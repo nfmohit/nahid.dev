c9cba277927958f72f575c56231a43d3186e7506
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-18 23:50:07 +0600

    Added plugins required for WordPress

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,10 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-source-wordpress`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

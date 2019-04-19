5f92cb2c13d40cd9ad92a1322fc761db06ec50af
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-19 13:49:17 +0600

    Applied updated indentation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,55 +1,63 @@
 // Get values for .env variables
 require( 'dotenv' ).config( {
-  path: '.env',
+	path: '.env',
 } );
 
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-wordpress`,
-      options: {
-        baseUrl: process.env.SOURCE_SITE,
-        protocol: 'https',
-        hostingWPCOM: false,
-        useACF: false,
-        searchAndReplaceContentUrls: {
-          sourceUrl: 'https://' + process.env.SOURCE_SITE,
-          replacementUrl: 'https://nfmohit.pro',
-        }
-      },
-    },    
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+
+	siteMetadata: {
+		title: `Gatsby Default Starter`,
+		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		author: `@gatsbyjs`,
+	},
+
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`,
+			},
+		},
+		`gatsby-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		{
+			resolve: `gatsby-source-wordpress`,
+			options: {
+				baseUrl: process.env.SOURCE_SITE,
+				protocol: 'https',
+				hostingWPCOM: false,
+				useACF: false,
+				searchAndReplaceContentUrls: {
+					sourceUrl: 'https://' + process.env.SOURCE_SITE,
+					replacementUrl: 'https://nfmohit.pro',
+				},
+				includedRoutes: [
+					"**/categories",
+					"**/posts",
+					"**/pages",
+					"**/media",
+					"**/tags",
+					"**/taxonomies",
+					"**/users",
+				],
+			},
+		},
+	],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,91 +1,67 @@
 /**
- * Create WordPress posts and pages
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+* Internal dependencies
+*/
+const _ = require( 'lodash' );
+const Promise = require( 'bluebird' );
+const path = require( 'path' );
+const slash = require( 'slash' );
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
-// Will create pages for WordPress pages (route : /{slug})
-// Will create pages for WordPress posts (route : /post/{slug})
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+/**
+* Implement the Gatsby API “createPages”
+*
+* Will create pages for Wordpress pages (route : /{slug})
+* Will create pages for Wordpress posts (route : /post/{slug})
+*/
 
-  // The “graphql” function allows us to run arbitrary
-  // queries against the local Gatsby GraphQL schema. Think of
-  // it like the site has a built-in database constructed
-  // from the fetched data that you can run queries against.
-  const result = await graphql(`
-    {
-      allWordpressPage {
-        edges {
-          node {
-            id
-            path
-            status
-            template
-          }
-        }
-      }
-      allWordpressPost {
-        edges {
-          node {
-            id
-            path
-            status
-            template
-            format
-          }
-        }
-      }
-    }
-  `)
+exports.createPages = ( {
+	graphql,
+	actions,
+} ) => {
 
-  // Check for any errors
-  if (result.errors) {
-    throw new Error(result.errors)
-  }
+	const {
+		createPage,
+	} = actions;
 
-  // Access query results via object destructuring
-  const { allWordpressPage, allWordpressPost } = result.data
+	return new Promise( ( resolve, reject ) => {
 
-  // Create Page pages.
-  const pageTemplate = path.resolve(`./src/templates/page.js`)
-  // We want to create a detailed page for each page node.
-  // The path field contains the relative original WordPress link
-  // and we use it for the slug to preserve url structure.
-  // The Page ID is prefixed with 'PAGE_'
-  allWordpressPage.edges.forEach(edge => {
-    // Gatsby uses Redux to manage its internal state.
-    // Plugins and sites can use functions like "createPage"
-    // to interact with Gatsby.
-    createPage({
-      // Each page is required to have a `path` as well
-      // as a template component. The `context` is
-      // optional but is often necessary so the template
-      // can query data specific to each page.
-      path: edge.node.path,
-      component: slash(pageTemplate),
-      context: {
-        id: edge.node.id,
-      },
-    })
-  })
+		/* Posts ( WordPress Native ) */
+		graphql(
+			`
+			{
+				allWordpressPost {
+					edges {
+						node {
+							id
+							slug
+							status
+							template
+							format
+						}
+					}
+				}
+			}
+			`,
 
-  const postTemplate = path.resolve(`./src/templates/post.js`)
-  // We want to create a detailed page for each post node.
-  // The path field stems from the original WordPress link
-  // and we use it for the slug to preserve url structure.
-  // The Post ID is prefixed with 'POST_'
-  allWordpressPost.edges.forEach(edge => {
-    createPage({
-      path: edge.node.path,
-      component: slash(postTemplate),
-      context: {
-        id: edge.node.id,
-      },
-    })
-  })
-}
+		).then( ( result ) => {
+
+			if ( result.errors ) {
+				console.log( result.errors );
+				reject( result.errors );
+			}
+			const postTemplate = path.resolve('./src/templates/post.js');
+
+			_.each( result.data.allWordpressPost.edges, ( edge ) => {
+
+				createPage( {
+					path: `/blog/${ edge.node.slug }`,
+					component: slash( postTemplate ),
+					context: {
+						id: edge.node.id,
+					},
+				} );
+			} );
+			resolve();
+		} );
+
+	} );
+};

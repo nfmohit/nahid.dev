9d961673f2854489b59ed742748081c20b549cb1
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-19 14:04:12 +0600

    Applied updated indentation

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,22 @@
+/**
+* Internal dependencies
+*/
 import React from "react"
 
+/**
+ * Project dependencies
+ */
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+
+	<Layout>
+		<SEO title="404: Not found" />
+		<h1>NOT FOUND</h1>
+		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+	</Layout>
+
 )
 
-export default NotFoundPage
+export default NotFoundPage;

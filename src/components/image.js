6c3a8f61e1a72876fa1a06dab460905286f86d71
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-19 14:20:01 +0600

    Applied updated indentation

--- src/components/image.js
+++ src/components/image.js
@@ -1,3 +1,6 @@
+/**
+* Internal dependencies
+*/
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
@@ -14,19 +17,23 @@ import Img from "gatsby-image"
  */
 
 const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
+
+	<StaticQuery
+		query=
+		{ graphql`
+			query {
+				placeholderImage: file( relativePath: { eq: "gatsby-astronaut.png" } ) {
+					childImageSharp {
+						fluid( maxWidth: 300 ) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+			}
+		`}
+		render={ data => <Img fluid={ data.placeholderImage.childImageSharp.fluid } /> }
+	/>
+
 )
-export default Image
+
+export default Image;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,61 @@
 /**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+* Layout component that queries for data
+* with Gatsby's StaticQuery component
+*
+* See: https://www.gatsbyjs.org/docs/static-query/
+*/
 
+/*
+* Internal dependencies
+*/
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+/*
+* Project dependencies
+*/
 import Header from "./header"
-import "./layout.css"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+const Layout = ( { children } ) => (
+
+	<StaticQuery
+		query=
+		{ graphql`
+			query SiteTitleQuery {
+				site {
+					siteMetadata {
+						title
+					}
+				}
+			}
+		`}
+		render={ data => (
+			<>
+				<Header siteTitle={ data.site.siteMetadata.title } />
+				<div
+					style={ {
+						margin: `0 auto`,
+						maxWidth: 960,
+						padding: `0px 1.0875rem 1.45rem`,
+						paddingTop: 0,
+					} }
+				>
+					<main>{ children }</main>
+					<footer>
+						© {new Date().getFullYear()}, Built with
+						{` `}
+						<a href="https://www.gatsbyjs.org">Gatsby</a>
+					</footer>
+				</div>
+			</>
+		) }
+	/>
+
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default Layout;

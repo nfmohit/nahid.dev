5d067d160bc4af04665c2c1dd381d44ac87132df
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-19 14:40:45 +0600

    Updated sitemetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ require( 'dotenv' ).config( {
 module.exports = {
 
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`,
+		title: `Nahid Ferdous Mohit`,
+		description: `WordPress Sorcerer & Web Interface Developer`,
+		author: `@nfmohit`,
 	},
 
 	plugins: [

897db0b30dff6c5a172744bb7415934c1b015633
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-29 11:34:07 +0600

    Added preliminary about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,6 +10,11 @@ import Helmet from 'react-helmet';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+/**
+ * Images
+ */
+import avatar from "./../images/avatar.jpg"
+
 const IndexPage = () => (
 
 	<Layout>
@@ -21,6 +26,27 @@ const IndexPage = () => (
 		/>
 
 		<SEO title="About" keywords={ [ `gatsby`, `application`, `react` ] } />
+
+		<div className="vcard">
+			<div className="row">
+				<div className="col-md-3 avatar">
+					<img src={ avatar } alt="Avatar" />
+				</div>
+				<div className="col-md-5 identity">
+					<h1>Nahid Ferdous Mohit</h1>
+				</div>
+				<div className="col-md-4 info">
+					<span>born</span>
+					<p>28<sup>th</sup> April, 1999</p>
+					<span>education</span>
+					<p>high school (2017) dropout</p>
+					<span>work</span>
+					<p>tech support engineer @ <a href="https://premium.wpmudev.org" target="_blank" rel="noopener noreferrer">WPMU DEV</a></p>
+					<span>location</span>
+					<p>Dhaka, Bangladesh</p>
+				</div>
+			</div>
+		</div>
 
 	</Layout>
 

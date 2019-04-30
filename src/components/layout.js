ad6990393015486f4cf1204d3177554a0d0d2866
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-30 10:53:55 +0600

    Added social icons to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,6 +20,7 @@ import Header from "./header"
 /*
  * Import Assets
  */
+import './../../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import '../assets/sass/main.scss';
 import '../assets/js/main.js';
 
@@ -46,12 +47,39 @@ const Layout = ( { children } ) => (
 				</main>
 				<footer>
 					<div className="container">
-						© {new Date().getFullYear()}, Built with
-						{` `}
-						<a href="https://www.gatsbyjs.org">Gatsby</a>,
-						Proudly powered by
-						{` `}
-						<a href="https://wordpress.org">WordPress</a>
+						<div className="row">
+							<div className="col-md-8">
+								© {new Date().getFullYear()}, Built with
+								{` `}
+								<a href="https://www.gatsbyjs.org">Gatsby</a>,
+								Proudly powered by
+								{` `}
+								<a href="https://wordpress.org">WordPress</a>
+							</div>
+							<div className="col-md-4 social-links">
+								<a href="https://github.com/nfmohit-wpmudev" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-github"></i>
+								</a>
+								<a href="https://profiles.wordpress.org/nfmohit/" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-wordpress"></i>
+								</a>
+								<a href="https://linked.com/in/nfmohit/" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-linkedin-in"></i>
+								</a>
+								<a href="https://twitter.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-twitter"></i>
+								</a>
+								<a href="https://www.instagram.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-instagram"></i>
+								</a>
+								<a href="https://facebook.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+									<i className="fab fa-facebook-f"></i>
+								</a>
+								<a href="mailto:hello@nfmohit.pro" target="_blank" rel="noopener noreferrer">
+									<i className="far fa-envelope"></i>
+								</a>
+							</div>
+						</div>
 					</div>
 				</footer>
 			</>

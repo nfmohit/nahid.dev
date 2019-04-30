e18e4b3fd58294b0c00efa0d7d50dd5f4c94b20a
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-04-30 10:53:31 +0600

    Added social icons to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,6 +34,29 @@ const IndexPage = () => (
 				</div>
 				<div className="col-md-5 identity">
 					<h1>Nahid Ferdous Mohit</h1>
+					<div className="social-links">
+						<a href="https://github.com/nfmohit-wpmudev" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-github"></i>
+						</a>
+						<a href="https://profiles.wordpress.org/nfmohit/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-wordpress"></i>
+						</a>
+						<a href="https://linked.com/in/nfmohit/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-linkedin-in"></i>
+						</a>
+						<a href="https://twitter.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-twitter"></i>
+						</a>
+						<a href="https://www.instagram.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-instagram"></i>
+						</a>
+						<a href="https://facebook.com/nfmohit/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-facebook-f"></i>
+						</a>
+						<a href="mailto:hello@nfmohit.pro" target="_blank" rel="noopener noreferrer">
+							<i className="far fa-envelope"></i>
+						</a>
+					</div>
 				</div>
 				<div className="col-md-4 info">
 					<span>born</span>

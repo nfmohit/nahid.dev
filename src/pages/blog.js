eb57a7859b7ac43c2fc9338555d692968208d0b6
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-05-03 14:09:21 +0600

    Removed Featured Image from Blog Archive page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,6 @@
 * Internal dependencies
 */
 import React from 'react';
-import Img from 'gatsby-image';
 import { StaticQuery, graphql, Link } from 'gatsby';
 
 /**
@@ -54,16 +53,6 @@ const Blog = () => (
 							<div className="blog-grids">
 								{ data.allWordpressPost.edges.map( ( { node } ) => (
 									<div key={ node.slug } className="grid">
-										<div className="entry-media">
-											{ node.featured_media && (
-												<Img
-													src={node.featured_media.localFile.childImageSharp.sizes.src}
-													sizes={node.featured_media.localFile.childImageSharp.sizes}
-													alt="blog_image_one"
-													className="img-fluid"
-												/>
-											) }
-										</div>
 										<div className="entry-body">
 											<span className="cat">{ node.categories && node.categories.map( category => `${ category.name }, `) }</span>
 											<h3><Link to={ `/blog/${ node.slug }` } dangerouslySetInnerHTML={ { __html: node.title } } /></h3>

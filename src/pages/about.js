2eefe3bd88fe0ec6899368c6f8a827149ea418a3
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-05-06 10:12:33 +0600

    Updated biography

--- src/pages/about.js
+++ src/pages/about.js
@@ -66,15 +66,16 @@ const AboutPage = () => (
 					<span>work</span>
 					<p>tech support engineer @ <a href="https://premium.wpmudev.org" target="_blank" rel="noopener noreferrer" className="inline-link">WPMU DEV</a></p>
 					<span>location</span>
-					<p>Dhaka, Bangladesh</p>
+					<p>Gazipur, Bangladesh</p>
 				</div>
 			</div>
 		</div>
 
 		<div className="biography">
 			<h2>About Nahid</h2>
-			<p>Nahid is a 20 years old software developer located in Dhaka, Bangladesh. He is currently working as a tech support engineer at <a href="https://premium.wpmudev.org/" className="inline-link" target="_blank" rel="noopener noreferrer">WPMU DEV</a>, one of the leading WordPress development companies.
-			</p>
+			<p>Nahid is a 20 years old, self-learned software developer, currently working as a tech support engineer at <a href="https://premium.wpmudev.org/" className="inline-link" target="_blank" rel="noopener noreferrer">WPMU DEV</a>, one of the leading WordPress development companies. Besides his awesome day job, he spends the rest of his leisure time in open source development. <strong>Spoiler alert:</strong> He has a special place in his heart for Javascript.</p>
+			<h3>Biography</h3>
+			<p>Nahid was born on 28<sup>th</sup> April of 1999 in the beautiful city of Gazipur in Bangladesh, where he currently lives with his family. He got his first computer at the age of 10 from his father. It was mostly used for playing video games, thanks to the widespread "unavailablity" of the Internet. What used to make him thrill more than the fun playing games was their mechanism, e.g. how it was working, how a tap in the keyboard could get so many "physically untouchable" things done. When he got an Internet connection, he would research day and night in order to find the answers of his questions, gradually turning himself into a clog of the machines. Over a couple failed pet projects in school, he came to know about WordPress and fell in love with it.</p>
 		</div>
 
 	</Layout>

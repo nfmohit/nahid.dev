4d87c6cca4c27aa0efa84c33a64b8b79a1e164ca
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-05-12 08:45:10 +0600

    Added single project template

---
+++ src/templates/project.js
@@ -0,0 +1,58 @@
+/**
+* Internal dependencies
+*/
+import React from 'react';
+import PropType from 'prop-types';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+
+/**
+ * Project dependencies
+ */
+import Layout from '../components/layout';
+
+const ProjectTemplate = (props) => {
+
+	const { data: { wordpressWpProjects: post } } = props;
+
+	return (
+		<Layout>
+
+			<Helmet
+				title={ post.title }
+				meta={ [
+					{ name: 'description', content: post.excerpt },
+				] }
+			/>
+
+			<article>
+
+				<header>
+					<h1 dangerouslySetInnerHTML={ { __html: post.title } } />
+				</header>
+
+				<div className="content-holder">
+					<div className="content-body">
+						<p dangerouslySetInnerHTML={ { __html: post.content } } />
+					</div>
+				</div>
+			</article>
+
+		</Layout>
+	);
+};
+
+ProjectTemplate.propTypes = {
+	data: PropType.shape( {} ).isRequired,
+};
+export default ProjectTemplate;
+
+export const pageQuery = graphql`
+query($id: String!) {
+	wordpressWpProjects( id: { eq: $id } ) {
+		title
+		content
+		slug
+	}
+}
+`;

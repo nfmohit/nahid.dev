a4ddc3172937856645c1d6c68202d96b65b244ee
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-05-17 14:07:31 +0600

    Introduced project archive page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,29 +1,54 @@
 /**
 * Internal dependencies
 */
-import React from "react"
-import Helmet from 'react-helmet';
+import React from 'react';
+import { StaticQuery, graphql, Link } from 'gatsby';
 
 /**
  * Project dependencies
  */
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
 
-const ProjectsPage = () => (
+const Projects = () => (
 
-	<Layout>
+	<StaticQuery
+		query=
+			{ graphql`
+				query {
+					allWordpressWpProjects(sort: { fields: [date], order:DESC }) {
+						edges {
+							node {
+								title
+								slug
+							}
+						}
+					}
+				}
+			`}
 
-		<Helmet
-			bodyAttributes={{
-		        class: 'projects'
-		    }}
-		/>
+		render={ data => (
+			<Layout>
+				<div className="container">
+					<div className="row">
+						<div className="blog-grids">
+							{ data.allWordpressWpProjects.edges.map( ( { node } ) => (
+								<div key={ node.slug } className="grid">
+									<div className="entry-body">
+										<span className="cat">{ node.categories && node.categories.map( category => `${ category.name }, `) }</span>
+										<h3><Link to={ `/projects/${ node.slug }` } dangerouslySetInnerHTML={ { __html: node.title } } /></h3>
+										<p dangerouslySetInnerHTML={ { __html: node.excerpt } } />
+										<div className="read-more-date">
+											<span className="date">{ node.date }</span>
+										</div>
+									</div>
+								</div>
+							) ) }
+						</div>
+					</div>
+				</div>
+			</Layout>)
+		}
+	/>
+);
 
-		<SEO title="Projects" keywords={ [ `gatsby`, `application`, `react` ] } />
-
-	</Layout>
-
-)
-
-export default ProjectsPage;
+export default Projects;

609e1c4bd5ccfea7f47458e553d69a946fb3353e
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-06-06 12:29:27 +0600

    Updated projects page markup

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -44,7 +44,12 @@ const Projects = () => (
 					{ data.allWordpressWpProjects.edges.map( ( { node } ) => (
 						<div key={ node.slug } className="col-sm-6">
 							<div className="project-entry">
-								<h4 className="entry-title"><Link to={ `/projects/${ node.slug }` } dangerouslySetInnerHTML={ { __html: node.title } } /></h4>
+								<h3 className="entry-title"><Link to={ `/projects/${ node.slug }` } dangerouslySetInnerHTML={ { __html: node.title } } /></h3>
+								<p className="entry-excerpt">Simple introduction to this project which gives the reader an idea of what it is.</p>
+								<div className="project-icons">
+									<a href="https://github.com/nfmohit-wpmudev/nahid.dev" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+									<a href="https://github.com/nfmohit-wpmudev/nahid.dev" target="_blank" rel="noopener noreferrer"><i className="fab fa-wordpress"></i></a>
+								</div>
 							</div>
 						</div>
 					) ) }

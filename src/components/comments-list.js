ba508116cbe8d62e6929282a6509b52b0eb68d46
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-26 00:37:12 +0600

    Started displaying comments

--- src/components/comments-list.js
+++ src/components/comments-list.js
@@ -4,12 +4,41 @@
 import React from 'react'
 
 const CommentsList = ( { comments } ) => (
-	<div>
-		{ comments.map( comment => (
-			<div>
-				<span>{ comment.content }</span>
-			</div>
-		) ) }
+	<div className="comments-list">
+		{ comments
+			.filter( comment => ! comment.wordpress_parent )
+			.map( comment => {
+				const child_comment = comments.find( c => comment.wordpress_id === c.wordpress_parent )
+				return (
+					<div key={ comment.id }>
+						<div className="comment">
+							<div className="author">
+								<img
+									src={ comment.author_avatar_urls.wordpress_48 }
+									alt={ comment.author_name }
+									className="author-avatar"
+								/>
+								<span className="author-name">{ comment.author_name }</span>
+							</div>
+							<div dangerouslySetInnerHTML={ { __html: comment.content } } />
+						</div>
+						{ child_comment && (
+							<div className="comment reply">
+								<div className="author">
+									<img
+										src={ child_comment.author_avatar_urls.wordpress_48 }
+										alt={ child_comment.author_name }
+										className="author-avatar"
+									/>
+									<span className="author-name">{ child_comment.author_name }</span>
+								</div>
+								<div dangerouslySetInnerHTML={ { __html: child_comment.content } } />
+							</div>
+						) }
+					</div>
+				)
+			} ) 
+		}
 	</div>
 )
 

353d0b1195b38be0054db21a2f745a7cca4347e4
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-28 22:53:32 +0600

    Introduced pulling of multiple child comments | introduced a list-style markup for comments

--- src/components/comments-list.js
+++ src/components/comments-list.js
@@ -3,49 +3,62 @@
 */
 import React from 'react'
 
-const CommentsList = ( { comments } ) => (
-	<div className="comments-list">
-		{ comments
-			.filter( comment => ! comment.wordpress_parent )
-			.map( comment => {
-				const child_comment = comments.find( c => comment.wordpress_id === c.wordpress_parent )
-				return (
-					<div key={ comment.id } className="comment-container">
-						<div className="comment">
-							<div className="author">
-								<img
-									src={ comment.author_avatar_urls.wordpress_48 }
-									alt={ comment.author_name }
-									className="author-avatar"
-								/>
-								<div className="comment-meta">
-									<div className="author-name">{ comment.author_name }</div>
-									<div className="comment-date">{ comment.date }</div>
-								</div>
-							</div>
-							<div className="comment-content" dangerouslySetInnerHTML={ { __html: comment.content } } />
-						</div>
-						{ child_comment && (
-							<div className="comment reply">
-								<div className="author">
-									<img
-										src={ child_comment.author_avatar_urls.wordpress_48 }
-										alt={ child_comment.author_name }
-										className="author-avatar"
-									/>
-									<div className="comment-meta">
-										<div className="author-name">{ child_comment.author_name }</div>
-										<div className="comment-date">{ child_comment.date }</div>
+const CommentsList = ( { comments } ) => {
+
+	return (
+		<div className="comments-list">
+			<ul className="comments-container">
+				{ comments
+					.filter( comment => ! comment.wordpress_parent )
+					.map( comment => {
+
+						const child_comments = comments.filter( child_comment => comment.wordpress_id === child_comment.wordpress_parent )
+
+						return (
+							<li key={ comment.id } className="comment-container">
+								<div className="comment">
+									<div className="author">
+										<img
+											src={ comment.author_avatar_urls.wordpress_48 }
+											alt={ comment.author_name }
+											className="author-avatar"
+										/>
+										<div className="comment-meta">
+											<div className="author-name">{ comment.author_name }</div>
+											<div className="comment-date">{ comment.date }</div>
+										</div>
 									</div>
+									<div className="comment-content" dangerouslySetInnerHTML={ { __html: comment.content } } />
 								</div>
-								<div className="comment-content" dangerouslySetInnerHTML={ { __html: child_comment.content } } />
-							</div>
-						) }
-					</div>
-				)
-			} ) 
-		}
-	</div>
-)
+								{ child_comments &&
+									<ul className="child-comments-container">
+										{ child_comments.map( child_comment => (
+											<li key={ child_comment.id } className="child-comment-container">
+												<div className="comment reply">
+													<div className="author">
+														<img
+															src={ child_comment.author_avatar_urls.wordpress_48 }
+															alt={ child_comment.author_name }
+															className="author-avatar"
+														/>
+														<div className="comment-meta">
+															<div className="author-name">{ child_comment.author_name }</div>
+															<div className="comment-date">{ child_comment.date }</div>
+														</div>
+													</div>
+													<div className="comment-content" dangerouslySetInnerHTML={ { __html: child_comment.content } } />
+												</div>
+											</li>
+										) ) }
+									</ul>
+								}
+							</li>
+						)
+					} ) 
+				}
+			</ul>
+		</div>
+	)
+}
 
 export default CommentsList;

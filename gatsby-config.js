230077e83926463fea2c32eea9917cd2058cd6a8
nfmohit-wpmudev <nfmohit49@gmail.com>
2020-05-23 07:11:40 +0600

    Updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`
+		siteUrl: `https://nahid.dev`,
+		title: `Nahid Ferdous Mohit`,
+		description: `21-year-old self-taught software developer`,
+		author: `@nfmohit`
 	},
+
 	plugins: [
 		`gatsby-plugin-react-helmet`,
 		{
